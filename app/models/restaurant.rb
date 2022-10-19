c1ba600faf7eb57df8392962f8a756f73035bcae
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-10-18 22:03:45 -0400

    restaurant id addition

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,9 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews
+
+  validates :name, presence: true
+  validates :address, presence: true
+  validates :category, presence: true
+  validates :category, inclusion: { in: %w(chinese italian japanese french belgian),
+    message: "%{value} is not a valid size" }
 end
