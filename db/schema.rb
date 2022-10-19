ff6e3b42cb66186fc7408f5e822f2f12cf097b44
mosesnjoroge <mosesbnjoroge@outlook.com>
2022-10-18 20:56:25 -0400

    reviews model created

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_19_004650) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_005402) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
     t.string "phone_number"
     t.string "category"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "content"
+    t.integer "rating"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
